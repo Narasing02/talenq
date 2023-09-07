c0f009fd7a97a47a75c0abdaf61618fa7e3627e6
Narasing02 <murapakanarasingarao02@gmail.com>
2023-09-07 13:52:12 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import Dashboard from "./Components/Dashboard";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+     <Dashboard />
+       
     </>
   );
 }
